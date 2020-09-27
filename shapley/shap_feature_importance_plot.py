b7916d37e436549210f5e40dddc3f5e44fdc9b41
Samuel Kim <46306493+samskim@users.noreply.github.com>
2020-09-27 18:04:55 -0400

    Update shap_feature_importance_plot.py

--- shapley/shap_feature_importance_plot.py
+++ shapley/shap_feature_importance_plot.py
@@ -4,26 +4,10 @@ import os
 import sys
 import xgboost as xgb
 import cPickle as cPickle
-import sklearn
-from xgboost.sklearn import XGBClassifier
-from sklearn import model_selection #for splitting data
-from sklearn.metrics import roc_curve, auc, average_precision_score, precision_recall_curve
-from sklearn.metrics import accuracy_score, precision_score,recall_score
-from sklearn import model_selection
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.ensemble import GradientBoostingClassifier
-from sklearn.ensemble import AdaBoostClassifier
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.model_selection import RandomizedSearchCV
-from sklearn.linear_model import LogisticRegression
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
-import scipy.stats as st
-from xgboost import plot_tree
-from graphviz import Graph
 import shap
-#note: not all imported packages are used
 
 modelinput = sys.argv[1] #.cPickle model file
 traininginput = sys.argv[2] #.tsv.gz feature matrix file
